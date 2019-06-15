857ad36da57f7b5059e153320f599f08ad9eec37
michebble <32097294+michebble@users.noreply.github.com>
2019-06-15 15:58:11 +1000

    Display schedule create

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Actions for Schedules
+class SchedulesController < ApplicationController
+  def create
+  end
+end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Schedule determines the amount of sessions and amount of pullups in sets before the next test
+# Schedule determines the amount of sessions and amount of pullups in sets
 class Schedule < ApplicationRecord
   belongs_to :user, inverse_of: :schedules
 end
