2a71d44ee617d36efc6a23f4a5ef4000331912f1
michebble <32097294+michebble@users.noreply.github.com>
2019-06-19 20:08:03 +1000

    Add level to schedule

--- db/migrate/20190615041635_create_schedules.rb
+++ db/migrate/20190615041635_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration[5.2]
   def change
     create_table :schedules do |t|
       t.belongs_to :user, index: true
-      t.string :name
+      t.integer :level
       t.timestamps
     end
   end
