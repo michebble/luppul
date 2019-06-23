9ae659879abfa9610d4937c6d6299f1d72921d04
michebble <32097294+michebble@users.noreply.github.com>
2019-06-23 12:30:39 +1000

    Create test of numericality

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,11 @@
 
 # Schedule determines the amount of sessions and amount of pullups in sets
 class Schedule < ApplicationRecord
-  validates :level, presence: true
+  validates :level, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 11
+  }
 
   belongs_to :user, inverse_of: :schedules
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Schedule, type: :model do
 
   describe 'Validations' do
     it { should validate_presence_of :level }
+    it do
+      should validate_numericality_of(:level)
+        .only_integer
+        .is_greater_than_or_equal_to(1)
+        .is_less_than_or_equal_to(11)
+    end
 
     it 'has a valid factory' do
       schedule = create(:schedule)
